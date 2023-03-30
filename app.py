ec9f6141731f14bc6e15afa3108eb366a245a514
Maxime_Ancl41 <maxime.ancelin41200@gmail.com>
2023-03-30 10:33:35 +0200

    Recupérer index.html

--- app.py
+++ app.py
@@ -10,17 +10,23 @@ if not Path('database/spotifaux.db').exists():
     sql = Path('tables.sql').read_text()
     conn.executescript(sql)
 
+# définir la constante pour la colonne "username"
+USR_USERNAME = "USR_USERNAME"
+
 @app.route('/')
 def users():
-    # se connecter à la base de données
+    # se connecter à la base de données 
     conn = sqlite3.connect('database/spotifaux.db')
 
     # récupérer les utilisateurs dans la table USER_USR
     cursor = conn.execute('SELECT * FROM USER_USR')
     users = cursor.fetchall()
 
+    # obtenir les noms de colonnes de la requête SQL
+    column_names = [description[0] for description in cursor.description]
+
     # fermer la connexion à la base de données
     conn.close()
 
     # afficher les utilisateurs dans une page HTML
-    return render_template('index.html', users=users)
+    return render_template('index.html', users=users, column_names=column_names)
